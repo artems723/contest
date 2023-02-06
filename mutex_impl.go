0144311c08a5621404936d8071b46d65eb1577a4
artem723 <artem723@gmail.com>
2023-02-07 00:42:46 +0300

    add mutex implementation

--- mutex_impl.go
+++ mutex_impl.go
@@ -1,5 +1,26 @@
 package contest
 
+type MyMutex struct {
+	lockChan chan struct{}
+}
+
 func New() Mutex {
-	return ... 
+	l := make(chan struct{}, 1)
+	l <- struct{}{}
+	m := MyMutex{
+		lockChan: l,
+	}
+	return &m
+}
+
+func (m *MyMutex) Lock() {
+	<-m.lockChan
+}
+
+func (m *MyMutex) Unlock() {
+	m.lockChan <- struct{}{}
+}
+
+func (m *MyMutex) LockChannel() <-chan struct{} {
+	return m.lockChan
 }
